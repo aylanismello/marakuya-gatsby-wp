e3e103fb136e07d8645f33b91a9680ee2b05f770
Aylan Mello <aylanismello@gmail.com>
2019-02-10 15:26:39 +0800

    ok

---
+++ src/templates/post.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Post extends React.Component {
+  render() {
+    // gist.github.com/lancejpollard/1978404
+    // https: // https://www.splashcopywriters.com/blog/meta-description
+    return (
+      <div style={{ background: '#fff' }}>
+      </div>
+    );
+  }
+}
+
+export default Post;
